0517552e897dbeff00ffd6a3132c2183bd365e9c
Ausinovich Kanstantsin <smilekostik@yandex.ru>
2019-07-15 18:20:54 +0300

    Initial Commit

---
+++ KeyboardDev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeyboardDev
+//
+//  Created by Яблочек on 15/07/2019.
+//  Copyright © 2019 Яблочек Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
